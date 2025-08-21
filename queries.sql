114fd5003e93ec8eaf7ac6727fbf47bd218257a0
mwly-01 <mlzardila@gmail.com>
2025-08-21 08:52:30 -0500

    feat: :sparkles: se esta terminanodo la de datos

--- queries.sql
+++ queries.sql
@@ -1,5 +1,29 @@
 -- Active: 1755090228603@@127.0.0.1@5433@miscompras@examen
 
---
-SELECT * FROM productos
-WHERE cantidad_stock > 5
+--Ejercicio 1
+    SELECT * ,'$' || TO_CHAR(precio_venta, 'FM999G999G999D00') AS precio_formateado
+    FROM productos
+    WHERE cantidad_stock < 5 
+
+--Ejercicio 2
+    SELECT 
+        DATE_TRUNC('month', co.fecha) AS mes,
+        COUNT(DISTINCT co.id_compra) AS num_compras,
+        SUM(cp.total) AS total_ventas
+    FROM compras co
+    JOIN compras_productos cp USING (id_compra)
+    WHERE cp.estado = 1
+    GROUP BY DATE_TRUNC('month', co.fecha)
+    ORDER BY mes;
+
+--Ejercicio 3
+    SELECT 
+    c.nombre ||' ' ||  c.apellidos AS nombre_completo_cliente,
+    SUM(cp.total) AS gasto_total,
+    RANK() OVER (ORDER BY SUM(cp.total) DESC) AS ranking_gasto
+FROM clientes c
+JOIN compras co USING (id_cliente)
+JOIN compras_productos cp USING (id_compra)
+WHERE cp.estado = 1
+GROUP BY c.id, c.nombre, c.apellidos
+ORDER BY gasto_total DESC;
